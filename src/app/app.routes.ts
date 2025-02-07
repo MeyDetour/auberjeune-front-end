12cef8b81635f39c88a60dacff7ce383356d1352
mey <meydetour@gmail.com>
2025-02-07 12:00:41 +0100

    feat : home page start : login page admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import {HomeComponent} from './pages/home/home.component';
+import {AuthComponent} from './pages/admin/auth/auth.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path : "",
+    component : HomeComponent
+  },  {
+    path : "admin/auth",
+    component : AuthComponent
+  },
+];
---
+++ src/app/services/user.service.ts
@@ -0,0 +1,10 @@
+import {inject, Injectable} from '@angular/core';
+import {BehaviorSubject} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+@Injectable({
+  providedIn: 'root',
+
+})
+export class UserService {
+
+}
