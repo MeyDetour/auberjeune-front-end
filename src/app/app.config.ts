671a5b77b646f24f0aa1b2f033253a1a3337e8e0
mey <meydetour@gmail.com>
2025-02-07 15:59:05 +0100

    feat : error handler

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,16 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import {provideHttpClient} from '@angular/common/http';
+import {provideHttpClient, withInterceptors} from '@angular/common/http';
+import {AuthInterceptor} from './interceptor/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideHttpClient(),
+    provideHttpClient(
+      withInterceptors([AuthInterceptor]),
+    ),
     provideRouter(routes)
+
   ]
 };
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,10 +1,53 @@
 import {inject, Injectable} from '@angular/core';
-import {BehaviorSubject} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
+import {BehaviorSubject, catchError, throwError} from 'rxjs';
+import {HttpClient, HttpErrorResponse} from '@angular/common/http';
+import {env} from '../environment/environment';
+import {User} from '../model/User.type';
+
+
+interface ApiTokenResponse {
+  token: string;
+}
+
 @Injectable({
   providedIn: 'root',
 
 })
 export class UserService {
+  private errorSource = new BehaviorSubject<string>("");
+  error$ = this.errorSource.asObservable();
+
+  private tokenSource = new BehaviorSubject<string | null>(null);
+  token$ = this.tokenSource.asObservable();
+
+  setToken(token: string | null) {
+    this.tokenSource.next(token);
+  }
+  setError(error: string) {
+    this.errorSource.next(error);
+  }
+
+  private baseUrl: string = env.apiUrl;
+
+  constructor(private http: HttpClient) {
+  }
+
+
+  async login(user: User) {
+    console.log(this.baseUrl + "api/login_check");
+    console.log(user)
+    return this.http.post<ApiTokenResponse>(this.baseUrl + "api/login_check", {
+      username: user.email,
+      password: user.password
+    }).subscribe(response => {
+        let token = response.token
+        localStorage.setItem("token", token);
+        this.setToken(token);
+      }
+    )
+  }
+
+
+
 
 }
